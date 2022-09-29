07fee96dc5aa6201a63f2f1b8701b5e31375fb56
aziz muhammad naufal <ajosnaufal@gmail.com>
2022-09-29 12:45:13 +0700

    useContext hook for theme toggle

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import { useState } from 'react';
-import './App.css';
-import TitleList from './components/TitileList';
+import { useState } from "react";
+import "./App.css";
+import TitleList from "./components/TitileList";
+import {
+  ThemeProvider,
+} from "./contexts/theme-context";
 
 const App = () => {
-  const [resourceName, setResourceName ] = useState('posts')
-
-
+  const [resourceName, setResourceName] = useState("posts");
   return (
     <>
-    <button onClick={() => {setResourceName('posts')}}>Posts</button>
-    <button onClick={() => {setResourceName('todos')}}>Todos</button>
-    <TitleList resourceName={resourceName} />
+      <ThemeProvider>
+        <button
+          onClick={() => {
+            setResourceName("posts");
+          }}
+        >
+          Posts
+        </button>
+        <button
+          onClick={() => {
+            setResourceName("todos");
+          }}
+        >
+          Todos
+        </button>
+        <TitleList resourceName={resourceName} />
+      </ThemeProvider>
     </>
   );
-}
+};
 
 export default App;
--- src/components/TitileList.jsx
+++ src/components/TitileList.jsx
@@ -1,19 +1,31 @@
 import useResources from "../hooks/useResources";
+import { useTheme, useThemeUpdate } from "../contexts/theme-context";
 
 const TitleList = ({ resourceName }) => {
   const titles = useResources(resourceName);
+  const darkTheme = useTheme()
+  const toggleTheme = useThemeUpdate()
+  const theme = {
+    backgroundColor: darkTheme ? '#333' : '#CCC',
+    color: darkTheme ? '#CCC' : '#333',
+    padding: '2rem',
+    margin: '2rem'
+  }
 
   return (
     <>
-      {titles && titles.length ? (
-        <ul>
-          {titles.map((item) => {
-            return <li key={item.id}>{item.title}</li>;
-          })}
-        </ul>
-      ) : (
-        <h1>kasep</h1>
-      )}
+      <button onClick={toggleTheme}>Toggle theme</button>
+      <div style={theme}>
+        {titles && titles.length ? (
+          <ul>
+            {titles.map((item) => {
+              return <li key={item.id}>{item.title}</li>;
+            })}
+          </ul>
+        ) : (
+          <h1>kasep</h1>
+        )}
+      </div>
     </>
   );
 };
